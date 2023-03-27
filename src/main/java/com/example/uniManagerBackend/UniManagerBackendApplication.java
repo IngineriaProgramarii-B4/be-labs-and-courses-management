b4d284a7db90888c4c0961ea21e63b667e271a0a
andreea-olariu <83142073+andreea-olariu@users.noreply.github.com>
2023-03-27 17:30:07 +0300

    initial commit

---
+++ src/main/java/com/example/uniManagerBackend/UniManagerBackendApplication.java
@@ -0,0 +1,12 @@
+package com.example.uniManagerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniManagerBackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(UniManagerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uniManagerBackend/UniManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uniManagerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
