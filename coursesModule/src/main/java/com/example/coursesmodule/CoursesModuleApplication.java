857610fa0731a14bab2171fee64b171df5d8d551
Denisa Munteanu <mariadenisa2904@gmail.com>
2023-03-28 15:05:35 +0300

    +added module

---
+++ coursesModule/src/main/java/com/example/coursesmodule/CoursesModuleApplication.java
@@ -0,0 +1,18 @@
+package com.example.coursesmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
+
+@SpringBootApplication
+public class CoursesModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(CoursesModuleApplication.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", "8083"));
+        app.run(args);
+        
+    }
+
+}
---
+++ coursesModule/src/test/java/com/example/coursesmodule/CoursesModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursesmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
