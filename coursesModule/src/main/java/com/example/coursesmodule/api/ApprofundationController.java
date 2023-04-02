935f72b67e66495796e13c36a6a42c2e0d1c4edb
MateiTiplea <m_tiplea@yahoo.com>
2023-04-02 13:14:06 +0300

    # comentat_erori

--- coursesModule/src/main/java/com/example/coursesmodule/api/ApprofundationController.java
+++ coursesModule/src/main/java/com/example/coursesmodule/api/ApprofundationController.java
@@ -27,8 +27,8 @@ public class ApprofundationController {
 
     @PostMapping(path = "subjectId={id}/approfundations")
     public void addApprofundation(@PathVariable("id") int id, @Valid @NonNull @RequestBody Approfundation approfundation) {
-        approfundationService.getCourseById(id)
-                .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Course not found"));
+ /*       approfundationService.getCourseById(id)
+                .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Course not found"));*/
         if(approfundationService.addApprofundation(id, approfundation) == 0) {
             throw new ResponseStatusException(NOT_ACCEPTABLE, "Approfundation ID already exists");
         }
@@ -36,38 +36,40 @@ public class ApprofundationController {
 
     @GetMapping(path = "subjectId={id}/approfundations")
     public List<Approfundation> getApprofundations(@PathVariable("id") int id) {
-        Course course = approfundationService.getCourseById(id)
-                .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Course not found"));
-        return approfundationService.getApprofundations(course);
+        /*Course course = approfundationService.getCourseById(id)
+                .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Course not found"));*/
+        /*return approfundationService.getApprofundations(course);*/
+        return null;
     }
 
     @GetMapping(path = "subjectId={id}/approfundationId={approfundationId}")
     public Approfundation getApprofundationById(@PathVariable("id") int id,
                                                 @PathVariable("approfundationId") int approfundationId) {
-        Course course = approfundationService.getCourseById(id)
+       /* Course course = approfundationService.getCourseById(id)
                 .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Course not found"));
         return approfundationService.getApprofundationById(course, approfundationId)
-                .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Approfundation not found"));
+                .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Approfundation not found"));*/
+        return null;
     }
 
     @DeleteMapping(path = "subjectId={id}/approfundationId={approfundationId}")
     public void deleteApprofundationById(@PathVariable("id") int id,
                                          @PathVariable("approfundationId") int approfundationId) {
-        approfundationService.getCourseById(id)
+        /*approfundationService.getCourseById(id)
                 .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Course not found"));
         if(approfundationService.deleteApprofundationById(id, approfundationId) == 0) {
             throw new ResponseStatusException(NOT_FOUND, "Approfundation not found");
-        }
+        }*/
     }
 
     @PutMapping(path = "subjectId={id}/approfundationId={approfundationId}")
     public void updateApprofundationById(@PathVariable("id") int id,
                                          @PathVariable("approfundationId") int approfundationId,
                                          @Valid @NonNull @RequestBody Approfundation approfundation) {
-        approfundationService.getCourseById(id)
+        /*approfundationService.getCourseById(id)
                 .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Course not found"));
         if(approfundationService.updateApprofundationById(id, approfundationId, approfundation) == 0) {
             throw new ResponseStatusException(NOT_FOUND, "Approfundation not found");
-        }
+        }*/
     }
 }
